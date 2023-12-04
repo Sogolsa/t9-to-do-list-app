291ebce387fa4fbd0dc1ebee9041521e08f0c1d2
Sogol <108494457+Sogolsa@users.noreply.github.com>
2023-12-03 17:04:41 -0800

    t9-to-do-list-app
    jquery
    1.Add 2.cross out 3. delete 4.reorder

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function newItem() {
+  // Adding a new item to the list of items //
+  let list = $('#list');
+  let li = $('<li></li>');
+  list.append(li);
+
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert('You must write something!');
+  } else {
+    let list = $('#list');
+    list.append(li);
+  }
+
+  // Crossing out an item //
+  li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+  });
+
+  // Create delete button //
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  // Class Delete //
+  crossOutButton.on('click', deleteListItem);
+  function deleteListItem() {
+    li.addClass('delete');
+  }
+
+  // Reorder the items //
+  list.sortable();
+}
